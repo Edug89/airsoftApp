51fa77dfc7f13f835d2dc1043ee13ad3e23419fe
Eduardo Martinez <eduardo.martinez@petroprix.com>
2023-10-25 16:44:15 +0200

    first commit

---
+++ airsoftApp/airsoftApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airsoftApp
+//
+//  Created by Eduardo Martinez Palomino on 25/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airsoftApp/airsoftApp/airsoftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  airsoftAppApp.swift
+//  airsoftApp
+//
+//  Created by Eduardo Martinez Palomino on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct airsoftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
